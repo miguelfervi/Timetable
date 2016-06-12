f2c75a988230282c402d1b14600c6086ffef93f4
Miguel <miguelfervi@gmail.com>
2016-06-12 18:18:57 +0200

    Initial commit

---
+++ app/src/main/java/es/gidm/timetable/db/TaskContract.java
@@ -0,0 +1,17 @@
+package es.gidm.timetable.db;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Miguel on 12/06/2016.
+ */
+public class TaskContract {
+    public static final String DB_NAME = "es.gidm.Timetable.todolist.db";
+    public static final int DB_VERSION = 1;
+
+    public class TaskEntry implements BaseColumns {
+        public static final String TABLE = "tasks";
+
+        public static final String COL_TASK_TITLE = "title";
+    }
+}
